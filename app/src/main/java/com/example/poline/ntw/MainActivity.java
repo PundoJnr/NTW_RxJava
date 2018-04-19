81757edf49bb315966c1b83660aae15a040acd8a
ArthurPundo <pundoarthur@gmail.com>
2018-04-19 16:09:58 +0300

    initial commit

--- app/src/main/java/com/example/poline/ntw/MainActivity.java
+++ app/src/main/java/com/example/poline/ntw/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.poline.ntw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -13,6 +15,9 @@ import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
     TextView text;
+    RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private StockDataAdapter stockDataAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +31,14 @@ public class MainActivity extends AppCompatActivity {
                         text.setText(s);
                     }
                 });
+        recyclerView = findViewById(R.id.recycler);
+
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        stockDataAdapter = new StockDataAdapter();
+        recyclerView.setAdapter(stockDataAdapter);
+
     }
 }
