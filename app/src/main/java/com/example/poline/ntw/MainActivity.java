419b27d847eaf7de6d6618b6aa2bbd33ff9e7580
ArthurPundo <pundoarthur@gmail.com>
2018-04-19 15:44:29 +0300

    initial commit

--- app/src/main/java/com/example/poline/ntw/MainActivity.java
+++ app/src/main/java/com/example/poline/ntw/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.poline.ntw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.function.Consumer;
+
+import io.reactivex.Observable;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text1);
+        Observable.just("Hello! Please use this app responsibly")
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        text.setText(s);
+                    }
+                });
     }
 }
